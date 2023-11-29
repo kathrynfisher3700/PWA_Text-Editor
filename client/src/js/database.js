1c213a5cf05ac6ea0f9295b06643f9c058d8dcaa
Aprecity <kathrynfisher3700@gmail.com>
2023-11-28 20:37:48 -0500

    updated database.js with putDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Exports the function to the database
+export const putDb = async (content) =>  {
+// Creates a connection to the Jate database and version.
+const jateDb = await openDB("jate", 1);
+
+// Creates new transaction and specifies the database and data privileges.
+const tx = jateDb.transaction("jate", "readwrite");
+
+// Opens up the desired object store.
+const store = tx.objectStore("jate");
+
+// Uses the put method on the store and passes in the content.
+const request = store.put({ id: 1, value: content });
+
+// Gets confirmation of the request.
+const result = await request;
+console.log("Data saved to the database:", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
